cf137bf80e87a155f16f08aecee1ca382374a4d0
grappl47 <shaunx0407@gmail.com>
2026-06-21 19:33:47 +0800

    water trail and rippling and bend(maybe havent tested)

--- ACCURSED/Assets/ShadowStuff/LightingScripts/WaterTrail.cs
+++ ACCURSED/Assets/ShadowStuff/LightingScripts/WaterTrail.cs
@@ -1,46 +1,117 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class WaterTrail : MonoBehaviour
 {
     LineRenderer lineRenderer;
-    [SerializeField] float minWidth;
-    [SerializeField] float widthIncreaseRate;
-    [SerializeField] float trailDuration;
+    [SerializeField] float minWidth = 1f;
+    [SerializeField] float widthIncreaseRate=3f;
+    [SerializeField] float trailDuration = 1f;
 
-    float trailLength = 0;
-    Vector2 prevPosition;
+    float trailSquaredLength = 0;
+    Vector3 prevPosition;
+
     Queue<Vector3> positions = new Queue<Vector3>();
-    Queue<float> timeLeft = new Queue<float>();
-    float timePassed = 0;
+    Queue<float> timesLeft = new Queue<float>();
+    Queue<float> squaredDists = new Queue<float>();
+    
 
     float fadeBufferTime;
-    Vector3[] emptyPositionsArray = new Vector3[0];
+    float timePassed = 0;
+
+    bool inWater;
 
     private void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
         prevPosition = transform.position;
-        StartTrail();
     }
 
     public void StartTrail()
     {
         fadeBufferTime = trailDuration;
-        trailLength = 0;
-        lineRenderer.SetPositions(emptyPositionsArray);
         timePassed = 0;
+        trailSquaredLength = 0;
+        lineRenderer.positionCount = 0;
+
+        prevPosition = transform.position;
         positions = new Queue<Vector3>();
-        timeLeft = new Queue<float>();
+        positions.Enqueue(prevPosition);
+
+        timesLeft = new Queue<float>();
+        timesLeft.Enqueue(fadeBufferTime);
+
+        squaredDists = new Queue<float>();
+        squaredDists.Enqueue(0);
+
         lineRenderer.startWidth = minWidth;
         lineRenderer.endWidth = minWidth;
+
+        inWater = true;
     }
 
     void Update()
     {
-        float distTravelled = ((Vector2)transform.position - prevPosition).sqrMagnitude;
-        trailLength += distTravelled;
+        if (inWater)
+        {
+            //add new position
+            timesLeft.Enqueue(Time.deltaTime);
+            positions.Enqueue(transform.position);
 
-        lineRenderer.endWidth = widthIncreaseRate * trailLength;
+            float currDist = (transform.position - prevPosition).sqrMagnitude;
+            trailSquaredLength += currDist;
+            squaredDists.Enqueue(currDist);
+            prevPosition = transform.position;
+        }
+
+
+        //remove positions
+        if (timesLeft.Count > 0)
+        {
+            timePassed += Time.deltaTime;
+            while (timesLeft.Count > 0 && timePassed > timesLeft.Peek())
+            {
+                timePassed -= timesLeft.Dequeue();
+                Vector3 positionToRemove = positions.Dequeue();
+                trailSquaredLength -= squaredDists.Dequeue();
+            }
+        }
+
+        //set positions and width
+        lineRenderer.positionCount = positions.Count;
+        lineRenderer.SetPositions(positions.ToArray());
+        lineRenderer.startWidth = minWidth + widthIncreaseRate * Mathf.Sqrt( trailSquaredLength);
+    }
+
+    public void EndTrail()
+    {
+        inWater = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Water")
+        {
+            StartTrail();
+            //Debug.Log("start");
+        }
+    }
+
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Water")
+    //    {
+    //        Debug.Log("stay");
+    //    }
+    //}
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Water")
+        {
+            EndTrail();
+            //Debug.Log("end");
+        }
     }
 }
