7ec2142553efdc9debde331f8b6dc344c2f8a4ed
WYJustin1213 <wyjustin1213@outlook.com>
2026-07-23 01:43:10 -0400

    Fade black screen problem

--- ACCURSED/Assets/Scripts/UI/UI_EscMenu/SOExeQuit.cs
+++ ACCURSED/Assets/Scripts/UI/UI_EscMenu/SOExeQuit.cs
@@ -5,11 +5,28 @@ using UnityEngine.SceneManagement;
 [CreateAssetMenu(menuName = "Scriptable Objects/Quit Button")]
 public class SOExeQuit : EscMenuUIButtonSO
 {
+    private Transform player;
+    private string playerTag = "Player";
+
+    private void Awake()
+    {
+        if (player == null)
+            FindPlayerTarget();
+    }
+
+    private void FindPlayerTarget()
+    {
+        GameObject playerObject = GameObject.FindGameObjectWithTag(playerTag);
+
+        if (playerObject != null)
+            player = playerObject.transform;
+    }
+
     public override void Execute()
     {
         EscMenuController.Instance.CloseMenu();
 
         // quit to main menu
-        RoomTransitionWithoutPlayer.Instance.BeginTransition("StartMenu");
+        RoomTransitionManager.Instance.BeginTransition("StartMenu", "back", player);
     }
 }
