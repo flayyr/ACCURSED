c66cc99a4d3866d59af3068559b09eefdd073d8c
grappl47 <shaunx0407@gmail.com>
2026-09-03 19:09:37 -0400

    multi hit attacks
    using placeholder animation rn

--- ACCURSED/Assets/Scripts/PLAYER_Movment/PlayerAttacker.cs
+++ ACCURSED/Assets/Scripts/PLAYER_Movment/PlayerAttacker.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class PlayerAttacker : MonoBehaviour
 {
-    [SerializeField] ActionSO attack;
+    //[SerializeField] ActionSO attack;
+    [SerializeField] ActionSO[] attackList;
+
     ActionQueuer actionQueuer;
-    CharacterManager combatManager;
+    PlayerManager playerManager;
 
     private ActionInstance currAttackInstance;
+    int currentAtkIndex = 0;
 
     private void Start()
     {
         actionQueuer = GetComponent<ActionQueuer>();
-        combatManager = GetComponent<CharacterManager>();
+        playerManager = GetComponent<PlayerManager>();
     }
 
     //called by player controller, simply queues attacks
@@ -19,13 +22,33 @@ public class PlayerAttacker : MonoBehaviour
     {
         if (pressed)
         {
-            currAttackInstance = actionQueuer.QueueAction(attack);
+            if (currAttackInstance != null && playerManager.GetCurrAction() == currAttackInstance) {//player is currently attacking with this basic attack
+                ActionSO playingSO = currAttackInstance.actionSO;
+                QueueNextAttackInSequence(playingSO);
+            } else {
+                QueueAttack(0);
+            }
         }
         else if(currAttackInstance != null)
         {
             //skip wind when let go
             currAttackInstance.skipWindWhenQueued = true;
-            combatManager.SkipWind(currAttackInstance);
+            playerManager.SkipWind(currAttackInstance);
         }
+    }
+
+    private bool QueueNextAttackInSequence(ActionSO currSO) {
+
+        for (int i = 0; i < attackList.Length; i++) {
+            if(currSO == attackList[i]) {
+                QueueAttack((i + 1) % attackList.Length);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void QueueAttack(int index) {
+        currAttackInstance = actionQueuer.QueueAction(attackList[index]);
     }
 }
