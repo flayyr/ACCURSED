3340b1777a13f09fe93a405f4a3a05bca5e92064
grappl47 <shaunx0407@gmail.com>
2026-09-05 19:26:13 -0400

    player combo attacks anim + logic

--- ACCURSED/Assets/Scripts/PLAYER_Movment/ActionQueuer.cs
+++ ACCURSED/Assets/Scripts/PLAYER_Movment/ActionQueuer.cs
@@ -6,13 +6,19 @@ public class ActionInstance
 {
     public ActionSO actionSO;
     public float queueTime;
-    public bool skipWindWhenQueued;
+    public bool skipWindWhenQueued = false;
+    public bool played = false;
+    public float finishTime = -1;
 
     public ActionInstance(ActionSO actionSO, float currTime)
     {
         this.actionSO = actionSO;
         queueTime = currTime;
         skipWindWhenQueued = false;
+    }
+
+    public void UpdateQueueTime() {
+        queueTime = Time.time;
     }
 }
 
@@ -25,7 +31,8 @@ public class ActionQueuer : MonoBehaviour
 
     Queue<ActionInstance> actionQueue;
 
-    ActionInstance latestIntance;
+    ActionInstance lastQueuedInstance;
+    ActionInstance lastPlayedInstance;
 
     private void Awake()
     {
@@ -36,11 +43,10 @@ public class ActionQueuer : MonoBehaviour
     {
         ActionInstance instance = new ActionInstance(attack, Time.time);
         actionQueue.Enqueue(instance);
+        lastQueuedInstance = instance;
 
         //broadcasts event for character manager, which will process the next attack
         OnActionQueued?.Invoke();
-
-        latestIntance = instance;
 
         return instance;
     }
@@ -56,23 +62,47 @@ public class ActionQueuer : MonoBehaviour
         //if no actions in queue
         if (actionQueue.Count == 0)
         {
-            latestIntance = null;
+            if(!lastQueuedInstance.played)//if latest wasnt played, meaning it was dequeued
+                lastQueuedInstance = null;
             return null;
         }
 
-        ActionInstance nextInstance= actionQueue.Dequeue();
-
-        if (nextInstance == latestIntance)
-            latestIntance = null;
-
-        return nextInstance;
+        lastPlayedInstance = actionQueue.Dequeue();
+        return lastPlayedInstance;
     }
 
     public void ClearActions()
     {
         actionQueue.Clear();
-        latestIntance = null;
+        if (!lastQueuedInstance.played)//if latest wasnt played, meaning it was dequeued
+            lastQueuedInstance = null;
     }
 
-    public ActionInstance GetLatestAction() { return latestIntance; }
+    public ActionInstance GetLastQueuedInstance() {
+        if (lastQueuedInstance == null)
+            return null;
+
+        if(lastQueuedInstance.finishTime != -1) {//latest instance had finished playing;
+            if (lastQueuedInstance.finishTime + queueWindow < Time.time) {//it finished too long ago to be chained
+                lastQueuedInstance = null;
+                return null;
+            }
+        }
+
+        return lastQueuedInstance; 
+    }
+
+    public ActionInstance GetLastPlayedInstance() {
+        if (lastPlayedInstance == null)
+            return null;
+
+        if (lastPlayedInstance.finishTime != -1) {//latest instance had finished playing;
+            if (lastPlayedInstance.finishTime + queueWindow < Time.time) {//it finished too long ago to be chained
+                lastPlayedInstance = null;
+                return null;
+            }
+        }
+
+        return lastPlayedInstance;
+    }
 }
